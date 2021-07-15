f58d98e3400a6ccb39de9154c825f684e791b033
Omotola-Elujulo <elujuloomotola@gmail.com>
2021-07-15 17:12:15 -0500

    Flix Part 2
    Added Move Details about a particular movie, and a tab bar button to view a grid layout of Movie Posters using a CollectionView

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Omotola Elujulo on 7/14/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
